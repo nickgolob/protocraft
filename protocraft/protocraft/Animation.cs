ff893f151b9a2e424ae27edec186dc49ac8354dd
Nick Golob <nickgolob@gmail.com>
2016-05-07 21:28:35 -0700

    Revert "Revert "initial shit""
    This reverts commit edd4ce83664ade392a150605d3921ab70bc6f96d.

---
+++ protocraft/protocraft/Animation.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace protocraft
+{
+    public class Animation
+    {
+        List<AnimationFrame> frames = new List<AnimationFrame>();
+        TimeSpan timeIntoAnimation;
+
+        TimeSpan Duration
+        {
+            get
+            {
+                double totalSeconds = 0;
+                foreach (var frame in frames)
+                {
+                    totalSeconds += frame.duration.TotalSeconds;
+                }
+
+                return TimeSpan.FromSeconds(totalSeconds);
+            }
+        }
+
+        private AnimationFrame currentFrame
+        {
+            get
+            {
+                AnimationFrame currentFrame = null;
+
+                // See if we can find the frame
+                TimeSpan accumulatedTime = TimeSpan.Zero;
+                foreach (var frame in frames)
+                {
+                    if (accumulatedTime + frame.duration >= timeIntoAnimation)
+                    {
+                        currentFrame = frame;
+                        break;
+                    }
+                    else
+                    {
+                        accumulatedTime += frame.duration;
+                    }
+                }
+
+                // If no frame was found, then try the last frame, 
+                // just in case timeIntoAnimation somehow exceeds Duration
+                if (currentFrame == null)
+                {
+                    currentFrame = frames.LastOrDefault();
+                }
+
+                return currentFrame;
+            }
+        }
+
+        public Rectangle CurrentRectangle
+        {
+            get
+            {
+                AnimationFrame currentFrame = this.currentFrame;
+
+                // If we found a frame, return its rectangle, otherwise
+                // return an empty rectangle (one with no width or height)
+                if (currentFrame != null)
+                {
+                    return currentFrame.sourceRectangle;
+                }
+                else
+                {
+                    return Rectangle.Empty;
+                }
+            }
+        }
+
+        public Texture2D currentTexture
+        {
+            get
+            {
+                return this.currentFrame.spriteSheet.sheetTexture;
+            }
+        }
+
+        public void AddFrame(Rectangle rectangle, TimeSpan duration, sprites.SpriteSheet spriteSheet)
+        {
+            frames.Add(new AnimationFrame(rectangle, duration, spriteSheet));
+        }
+        public void AddFrame(AnimationFrame animationFrame)
+        {
+            frames.Add(animationFrame);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            double secondsIntoAnimation = timeIntoAnimation.TotalSeconds + gameTime.ElapsedGameTime.TotalSeconds;
+
+            double remainder = secondsIntoAnimation % Duration.TotalSeconds;
+            timeIntoAnimation = TimeSpan.FromSeconds(remainder);
+        }
+    }
+}
---
+++ protocraft/protocraft/sprites/Sprites.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace protocraft.sprites
+{
+    public static class SpriteSheetManager
+    {
+        private static SpriteSheet vx_sprites_;
+        public static SpriteSheet vx_sprites
+        {
+            get
+            {
+                if (vx_sprites_ == null)
+                {
+                    vx_sprites_ = new vx_sprites();
+                }
+                return vx_sprites_;
+            }
+        }
+    }
+
+    public abstract class SpriteSheet
+    {
+        protected abstract Color GetAlphaChannel();
+        protected abstract string GetSpritesheetPath();
+
+        public Texture2D sheetTexture { get; private set; }
+        public SpriteSheet()
+        {
+            using (var stream = TitleContainer.OpenStream(GetSpritesheetPath()))
+            {
+                sheetTexture = Texture2D.FromStream(Protocraft.self.GraphicsDevice, stream);
+            }
+        }
+    }
+
+    public class vx_sprites : SpriteSheet
+    {
+        protected override Color GetAlphaChannel() { return new Color(120, 195, 128); }
+        protected override string GetSpritesheetPath() { return "Content/vx_sprites/vx_sprites.png"; }
+    }
+}
